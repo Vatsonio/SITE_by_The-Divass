8b2b6a3c7b8d30a832e3cdc43e2e3d2e6f90dfc3
Vatsonio <85784360+Vatsonio@users.noreply.github.com>
2024-04-29 02:36:16 +0300

    Delete server directory

--- server/models/Subscription.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const SubscriptionSchema = new Schema({
-    applicationId: { type: Schema.Types.ObjectId, ref: 'Application', required: true },
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }
-});
-
-module.exports = mongoose.model('Subscription', SubscriptionSchema);
