8aea8993bae52d8a78ba98b2d47e957a9f688790
Vatsonio <85784360+Vatsonio@users.noreply.github.com>
2024-04-29 02:36:03 +0300

    Delete client directory

--- client/src/components/Form.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Form = ({ onSubmit, children }) => (
-    <form onSubmit={onSubmit}>
-        {children}
-    </form>
-);
-
-export default Form;
--- client/src/components/Input.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Input = ({ type, name, value, onChange }) => (
-    <input type={type} name={name} value={value} onChange={onChange} />
-);
-
-export default Input;
